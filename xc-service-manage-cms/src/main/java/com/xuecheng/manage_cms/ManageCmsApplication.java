8db469fe039c8e8c7042fe1a3756408bf008d1be
lusiwei <951571596@qq.com>
2019-01-10 23:07:43 +0800

    eureka

--- xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/ManageCmsApplication.java
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/ManageCmsApplication.java
@@ -3,6 +3,7 @@ package com.xuecheng.manage_cms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
@@ -17,6 +18,7 @@ import org.springframework.web.client.RestTemplate;
 @EntityScan("com.xuecheng.framework.domain.cms")
 @ComponentScan(basePackages = {"com.xuecheng.api"})
 @ComponentScan(basePackages = {"com.xuecheng.manage_cms"})
+@EnableDiscoveryClient
 public class ManageCmsApplication {
     public static void main(String[] args) {
         SpringApplication.run(ManageCmsApplication.class);
